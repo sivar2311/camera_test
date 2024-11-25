e7f0e00943da6d56b7f3ae9b82dd8d2a26f8ab10
sivar2311 <sivar2311@gmail.com>
2024-11-25 05:44:00 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,34 @@
 #include <Arduino.h>
-#include <HTTPClient.h>
+#include <WiFi.h>
 #include <SinricPro.h>
 #include <SinricProCamera.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
 
 #include "camera.h"
 
-const char* WIFI_SSID = "";
-const char* WIFI_PASS = "";
+const char* WIFI_SSID = "SinricWiFi";
+const char* WIFI_PASS = "SinricWiFi";
 
-const char* DEVICE_ID  = "";
-const char* APP_KEY    = "";
-const char* APP_SECRET = "";
+const char* DEVICE_ID  = "6741903cdb15b093899448af";
+const char* APP_KEY    = "ebf0cf46-97a2-4c4e-8cf3-e630e9b640ca";
+const char* APP_SECRET = "5690fc71-938c-4174-a1de-270c2c3cffef-ffc31556-4219-4043-bb59-d2f6ac372d21";
 
-bool shouldUploadImage = false;
+bool handleSnapshot(const String& deviceId) {
+    camera_fb_t* fb = esp_camera_fb_get();
 
-int uploadImage(camera_fb_t* fb, const String& server, const String& deviceId, unsigned long timestamp) {
-    Serial.println("Uploading... ");
+    if (!fb) {
+        Serial.println("Failed to grab image");
+        return false;
+    }
+    SinricProCamera& myCamera = SinricPro[deviceId];
 
-    if (!fb) return -1;
+    int result = myCamera.uploadImage(fb->buf, fb->len);
 
-    WiFiClientSecure client;
-    client.setInsecure();
+    esp_camera_fb_return(fb);
 
-    String signature = "blahblah";
-
-    Serial.print("Signature:");
-    Serial.println(signature);
-
-    HTTPClient http;
-    if (!http.begin(client, server, 443, "/snapshot", true)) return -1;
-    http.addHeader("deviceid", deviceId);
-    http.addHeader("timestamp", String(timestamp));
-    http.addHeader("signature", signature);
-    int resCode = http.POST(fb->buf, fb->len);
-    http.end();
-
-    Serial.print("Response Code:");
-    Serial.println(resCode);
-
-    return resCode;
+    return result = 200;
 }
 
-
 bool onPowerState(const String& deviceId, bool& state) {
-    if (!state) return true;
-
-    shouldUploadImage = true;
-
-    state = false;
     return true;
 }
 
@@ -68,28 +47,11 @@ void setupWiFi() {
 void setupSinricPro() {
     SinricProCamera& myCamera = SinricPro[DEVICE_ID];
     myCamera.onPowerState(onPowerState);
-
+    myCamera.onSnapshot(handleSnapshot);
     SinricPro.onConnected([]() { Serial.printf("Connected to SinricPro\r\n"); });
     SinricPro.onDisconnected([]() { Serial.printf("Disconnected from SinricPro\r\n"); });
 
-    SinricPro.begin(APP_KEY, APP_SECRET, "testws.sinric.pro");
-}
-
-void handleImageUpload() {
-    if (!shouldUploadImage) return;
-
-    camera_fb_t* fb = esp_camera_fb_get();
-
-    if (!fb) {
-        Serial.println("Failed to grab image");
-        return;
-    }
-
-    int res = uploadImage(fb, "testcamera.sinric.pro", DEVICE_ID, SinricPro.getTimestamp());
-    esp_camera_fb_return(fb);
-    Serial.println(res);
-
-    shouldUploadImage = false;
+    SinricPro.begin(APP_KEY, APP_SECRET);
 }
 
 void setup() {
@@ -101,5 +63,4 @@ void setup() {
 
 void loop() {
     SinricPro.handle();
-    handleImageUpload();
 }
